da0f52e7c73d831f7f52fb3d92e4dc90c5042756
Jonathan <jonathan.leritte@gmail.com>
2019-12-12 15:28:56 -0800

    Removed whitespace and unneeded variable declaration

--- crypto.py
+++ crypto.py
@@ -10,37 +10,27 @@ BE = default_backend()
 def derive_key(passphrase, salt):
   salt = salt if salt != None else os.urandom(16)
   dk = hashlib.pbkdf2_hmac("sha256",passphrase,salt,100000)
-
   return (salt,ba.b2a_hex(dk))
 
 def encrypt(key, plaintext, passphrase = ''):
   iv = os.urandom(12)
   encryptor = Cipher(algorithms.AES(key),modes.GCM(iv),BE).encryptor()
-
   if passphrase:
     encryptor.authenticate_additional_data(passphrase)
-
   ciphertext = encryptor.update(plaintext) + encryptor.finalize()
-
   return iv + encryptor.tag + ciphertext
 
 def decrypt(key, cipherblock, passphrase = ''):
   iv = cipherblock[:12]
   ciphertext = cipherblock[28:]
   tag = cipherblock[12:28]
-
   decryptor = Cipher(algorithms.AES(key),modes.GCM(iv,tag),BE).decryptor()
-
   if passphrase:
     decryptor.authenticate_additional_data(passphrase)
-
-  plaintext = ""
-
   try:
     plaintext = decryptor.update(ciphertext) + decryptor.finalize()
   except:
     plaintext = None
-
   return plaintext
 
 def unlock_record(key, passphrase, record):
